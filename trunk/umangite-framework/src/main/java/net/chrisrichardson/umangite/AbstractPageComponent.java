b7222efc0fb054cfcebb8af64bdf0ccca87e4e34
chris.e.richardson <chris.e.richardson@69f5ca3c-ae30-0410-b196-f7f31ab160f7>
2009-02-04 23:10:24 +0000

    added some handy base classes

---
+++ trunk/umangite-framework/src/main/java/net/chrisrichardson/umangite/AbstractPageComponent.java
@@ -0,0 +1,11 @@
+package net.chrisrichardson.umangite;
+
+public class AbstractPageComponent extends AbstractPage {
+
+	protected final String id;
+
+	public AbstractPageComponent(String id) {
+		this.id = id;
+	}
+
+}

6662b49415585d45232712087d6a3dfcf7b0cb10
chris.e.richardson <chris.e.richardson@69f5ca3c-ae30-0410-b196-f7f31ab160f7>
2009-02-04 23:10:24 +0000

    added some handy base classes

---
+++ umangite-framework/src/main/java/net/chrisrichardson/umangite/AbstractPage.java
@@ -0,0 +1,19 @@
+package net.chrisrichardson.umangite;
+
+
+public abstract class AbstractPage extends WebTestSupport {
+
+	@Override
+	protected String[] getConfigLocations() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getUmangiteConfigLocations() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
